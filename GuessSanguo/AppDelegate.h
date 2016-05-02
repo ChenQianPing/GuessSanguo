b1325cd579725b5e20176aeb303f2fd3c564b64b
ChenQianPing <chenqianping@chenqianpingdemacbook-air.local>
2016-05-02 23:24:25 +0800

    Initial Commit

---
+++ GuessSanguo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GuessSanguo
+//
+//  Created by ChenQianPing on 16/5/2.
+//  Copyright © 2016年 ChenQianPing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GuessSanguo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GuessSanguo
+//
+//  Created by ChenQianPing on 16/5/2.
+//  Copyright © 2016年 ChenQianPing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
